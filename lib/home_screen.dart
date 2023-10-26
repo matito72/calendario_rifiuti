6f8f2d4c7ca4bb210381e641cbfdfb41e0df997d
matito72 <matito72@gmail.com>
2023-10-26 22:30:31 +0200

    fix
    fix

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -20,10 +20,10 @@ class _HomeScreenState extends State<HomeScreen> {
    @override
   void initState() {
     super.initState();
-    cambiaImmagine(_selectedDate);
+    _cambiaImmagine(_selectedDate);
   }
 
-  void getData(context) async {
+  void _getData(context) async {
     var fDate = await showDatePicker(
       context: context,
       initialDate: _selectedDate,
@@ -32,25 +32,35 @@ class _HomeScreenState extends State<HomeScreen> {
     );
     //aggiornare lo stato
     if (fDate != null) {
-      setState(() => cambiaImmagine(fDate));
+      setState(() => _cambiaImmagine(fDate));
     }
   }
 
-  goToMonthSucc() {
-    if (_selectedDate.month < 12) {
-      _selectedDate = DateTime(_selectedDate.year, _selectedDate.month, _selectedDate.day + 15);
-      setState(() => cambiaImmagine(_selectedDate));
+  _goToMonthSucc() {
+    if (_selectedDate.month <= 12) {
+      int day = (_selectedDate.day >= 16) ? 1 : 17;
+      int month = (_selectedDate.day > 16) 
+        ? (_selectedDate.month != 12) ? (_selectedDate.month + 1) : 12
+        : _selectedDate.month;
+
+      _selectedDate = DateTime(_selectedDate.year, month, day);
+      setState(() => _cambiaImmagine(_selectedDate));
     }
   }
 
-  goToMonthPrec() {
-    if (_selectedDate.month > 1) {
-      _selectedDate = DateTime(_selectedDate.year, _selectedDate.month, _selectedDate.day - 15);
-      setState(() => cambiaImmagine(_selectedDate));
+  _goToMonthPrec() {
+    if (_selectedDate.month >= 1) {
+      int day = (_selectedDate.day >= 16) ? 1 : 17;
+      int month = (_selectedDate.day > 16) 
+        ? _selectedDate.month
+        : (_selectedDate.month != 1) ? (_selectedDate.month - 1) : 1;
+
+      _selectedDate = DateTime(_selectedDate.year, month, day);
+      setState(() => _cambiaImmagine(_selectedDate));
     }
   }
 
-  cambiaImmagine(var fDate) {
+  _cambiaImmagine(var fDate) {
     _selectedDate = fDate;
     int month = _selectedDate.month;
     int day = _selectedDate.day;
@@ -68,58 +78,73 @@ class _HomeScreenState extends State<HomeScreen> {
     final DateFormat df = DateFormat("dd/MM/yyyy");
 
     return SafeArea(
-      child: Scaffold(
-        body: DecoratedBox(
-          decoration: BoxDecoration(
-            border: Border.all(width: 1),
-            borderRadius: const BorderRadius.all(Radius.circular(6.0)),
-            color: Colors.lightGreen.shade100,
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(top: 10),
-            child: Center(
-              child: Column(
-                  children: <Widget>[
-                    Card(
-                      elevation: 10,
-                      color: Colors.green.shade700, 
-                      child: SizedBox(
-                        width: 350,
-                        height:  (MediaQuery.of(context).size.height * 7 / 100),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text(
-                              df.format(_selectedDate),
-                              style: const TextStyle(color: Colors.white, fontSize: 25),
-                            ),
-                            IconButton(
-                              icon: const Icon(Icons.date_range, size: 30, color: Colors.white),
-                              onPressed: () => getData(context),
-                            )
-                          ]
-                        ),
-                      ),
-                    ),
-                    const Padding(padding: EdgeInsets.only(top: 20)) ,
-                    InteractiveViewer(
-                      panEnabled: true,
-                      minScale: 1,
-                      maxScale: 2,
-                      child: Container(
-                        width: (MediaQuery.of(context).size.width * 95 / 100),
-                        height: (MediaQuery.of(context).size.height * 80 / 100),
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            scale: 0.9,
-                            image: AssetImage(_pathImage),
-                            fit: BoxFit.fill,
+      child: WillPopScope(
+        onWillPop: () async => false,
+        child: Scaffold(
+          body: DecoratedBox(
+            decoration: BoxDecoration(
+              border: Border.all(width: 1),
+              borderRadius: const BorderRadius.all(Radius.circular(6.0)),
+              color: Colors.lightGreen.shade100,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Center(
+                child: Column(
+                    children: <Widget>[
+                      Card(
+                        elevation: 10,
+                        color: Colors.green.shade700, 
+                        child: SizedBox(
+                          width: 350,
+                          height:  (MediaQuery.of(context).size.height * 7 / 100),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                df.format(_selectedDate),
+                                style: const TextStyle(color: Colors.white, fontSize: 25),
+                              ),
+                              IconButton(
+                                icon: const Icon(Icons.date_range, size: 30, color: Colors.white),
+                                onPressed: () => _getData(context),
+                              )
+                            ]
                           ),
                         ),
                       ),
-                    )
-                  ],
-                ),
+                      const Padding(padding: EdgeInsets.only(top: 20)) ,
+                      GestureDetector(
+                        onHorizontalDragEnd: (dragDetail) {
+                        if (goPrecSucc) {
+                          if (dragDetail.primaryVelocity == null) return;
+                            if (dragDetail.primaryVelocity! < 0) {
+                              _goToMonthSucc();
+                            } else {
+                              _goToMonthPrec();
+                            }
+                          }
+                        },
+                        child: InteractiveViewer(
+                          panEnabled: true,
+                          minScale: 1,
+                          maxScale: 2,
+                          child: Container(
+                            width: (MediaQuery.of(context).size.width * 95 / 100),
+                            height: (MediaQuery.of(context).size.height * 80 / 100),
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                scale: 0.9,
+                                image: AssetImage(_pathImage),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+              ),
             ),
           ),
         ),
